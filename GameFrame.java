eedd50eb148d21ad84ab532c3109be1beba20c9b
Teksonisme <37880085+Teksonisme@users.noreply.github.com>
2020-04-05 18:59:07 +0200

    Create GameFrame.java

---
+++ GameFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import java.awt.GraphicsConfiguration;
+
+public class GameFrame {
+	static GraphicsConfiguration gc;
+	public static void main(String[] args){
+		JFrame frame= new JFrame(gc);
+		frame.setTitle("Feu de forêt");
+		frame.setSize(1152,720);
+		frame.setResizable(false);
+		frame.setLocation(350,200);
+		frame.setVisible(true);
+	}
+}
